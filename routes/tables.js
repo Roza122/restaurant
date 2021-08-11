02e83c7a4729cdb491c0204ef28690b7193d98f0
Roza122 <88772298+Roza122@users.noreply.github.com>
2021-08-11 13:04:23 +0300

    Delete routes directory

--- routes/tables.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { bookTable } from "../controllers/tables.js";
-import isAuth from "./isAuth.js";
-
-const Route = express.Router();
-
-Route.post("/book-table", isAuth, bookTable);
-
-export default Route;
